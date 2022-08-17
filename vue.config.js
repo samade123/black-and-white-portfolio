7d12b7ff2cdad493f85314a8cf7673a47c7ec2ce
Samuel Ademola <samuel.ademola43@gmail.com>
2022-08-17 22:12:31 +0100

    vue config and gitignore

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+  filenameHashing: true,
+  chainWebpack: config => {
+    config.plugin("copy").use(CopyPlugin, [
+      {
+        patterns: [{ from: "src/api", to: "../api" }],
+      },
+    ]);
+  },
+  pwa: {
+    name: 'Portfolio by Sam',
+    themeColor: '#e6ebf4',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    // manifestOptions: {
+
+    // },
+
+    // configure the workbox plugin
+    // workboxPluginMode: 'InjectManifest',
+    // workboxOptions: {
+    //   // swSrc is required in InjectManifest mode.
+    //   swSrc: 'dev/sw.js',
+    //   // ...other Workbox options...
+    // }
+  }
+})
