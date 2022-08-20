119740853d3091840918ac6ffb2dbd31a4a44ba2
Samuel Ademola <samuel.ademola43@gmail.com>
2022-08-20 22:35:56 +0100

    adding line awesome

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,17 @@
   </nav> -->
   <div class="header">
     <div class="left">Dev by Sam</div>
-    <div class="right">Menu</div>
+    <div class="right">
+      <i class="las la-lg la-map-marker"></i>
+    </div>
   </div>
   <router-view />
 </template>
 
 <style lang="scss">
 @import "./../node_modules/normalize.css/normalize.css";
+@import "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css";
+
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
