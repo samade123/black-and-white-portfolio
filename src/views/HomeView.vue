ec045585770e56a5dd08fba5263deb1b68e899e3
Samuel Ademola <samuel.ademola43@gmail.com>
2022-08-21 22:14:02 +0100

    Basic layour of portfolio added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
     <div class="bio-section">
-      <img alt="Vue logo" src="../assets/me.jpeg" />
+      <div
+        class="img"
+        :style="{
+          backgroundImage: 'url(' + require('@/assets/me.jpeg') + ')',
+        }"
+      ></div>
       <div class="img-friend">
         <h1>Hi, I'm Sam and I'm a <span>Junior front-end developer</span></h1>
         <p>
@@ -16,6 +21,13 @@
     </div>
 
     <div class="work-section">
+      <!-- <h1
+        :style="{
+          backgroundImage: 'url(' + require('@/assets/background.png') + ')',
+        }"
+      >
+        Work
+      </h1> -->
       <h1>Work</h1>
       <p class="description">
         Check my commercial and non commercial projects. If you have any
@@ -23,7 +35,9 @@
       </p>
       <div class="work-array">
         <div class="work-pill" v-for="pill in workArray" :key="pill.name">
-          {{ pill.name }} ({{jobs.filter((job)=>job.tags.includes(pill.name)).length}})
+          {{ pill.name }} ({{
+            jobs.filter((job) => job.tags.includes(pill.name)).length
+          }})
         </div>
       </div>
       <div class="jobs-array">
@@ -48,20 +62,60 @@
       </div>
     </div>
 
-    <div class="resume"></div>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <div class="resume-section">
+      <h1>Resume</h1>
+      <div class="Summary">
+        I am a keen and avid code, who learned most of his front end
+        edevelopment skills in my time at Event Engineering as an Intern
+        developer. BUt I also Have plenty of coding expereince from completing
+        my 4 year (BEng) degree in Electronics and Computer Systems Engineering.
+      </div>
+
+      <div class="subtitle"><span>Education</span></div>
+      <div class="education-div">
+        <div class="education" v-for="course in education" :key="course.src">
+          {{ course.name }}
+        </div>
+      </div>
+      <div class="subtitle"><span>Work expereince</span></div>
+      <div class="experience-div">
+        <div class="education" v-for="job in experience" :key="job.name">
+          <div class="">{{ job.role }}</div>
+          <div class="">{{ job.name }}</div>
+          <div class="">{{ job.dateCompleted }}</div>
+        </div>
+      </div>
+      <div class="subtitle"><span>Skills and tools</span></div>
+      <div class="skills-div">
+        <div class="skill" v-for="skill in skills" :key="skill.name">
+          <div class="">{{ skill.name }}</div>
+        </div>
+      </div>
+      <div class="subtitle">Download my resume as a PDF File</div>
+      <button class="buttons">
+        PDF <i class="las la-cloud-download-alt"></i>
+      </button>
+      <h1 class="contact">Contact Me</h1>
+      <div class="contact-section">
+        <div class="phone"><i class="las la-phone"></i> +447427476129</div>
+        <div class="email">
+          <i class="las la-at"></i> samuel.ademola43+dev@gmail.com
+        </div>
+      </div>
+    </div>
+    <Modal :showModal="showModal" />
   </div>
 </template>
 
 <script>
 import { ref } from "@vue/reactivity";
 // @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
+import Modal from "@/components/Modal.vue";
 
 export default {
   name: "HomeView",
   components: {
-    // HelloWorld,
+    Modal,
   },
   setup(props) {
     const workArray = ref([
@@ -74,9 +128,9 @@ export default {
     ]);
 
     const removeAll = (tagArray) => {
-      const allIndex = tagArray.indexOf('All');
-      return tagArray.slice(allIndex+1)
-    }
+      const allIndex = tagArray.indexOf("All");
+      return tagArray.slice(allIndex + 1);
+    };
 
     const jobs = ref([
       {
@@ -112,7 +166,81 @@ export default {
         otherFeatures: ["Responsive Design"],
       },
     ]);
-    return { workArray, jobs, removeAll };
+
+    const education = [
+      {
+        name: "Modern JavaScript Fundamentals",
+        src: "https://code.tutsplus.com/courses/javascript-fundamentals",
+        dateCompleted: "2019",
+      },
+      {
+        name: "Introduction to HTML",
+        src: "https://code.tutsplus.com/courses/introduction-to-html",
+        dateCompleted: "2019",
+      },
+      {
+        name: "Introduction to Git and GitHub",
+        src: "https://code.tutsplus.com/courses/introduction-to-git-and-github",
+        dateCompleted: "2019",
+      },
+    ];
+
+    const experience = [
+      {
+        name: "Event engineering",
+        role: "Intern Developer",
+        dateCompleted: "2019-2020",
+      },
+    ];
+
+    const skills = [
+      {
+        name: "Vue",
+      },
+      {
+        name: "Javascript",
+      },
+      {
+        name: "CSS",
+      },
+      {
+        name: "SCSS",
+      },
+      {
+        name: "Electron",
+      },
+      {
+        name: "Figma",
+      },
+      {
+        name: "Git",
+      },
+      {
+        name: "Webpack",
+      },
+      {
+        name: "Rest API",
+      },
+      {
+        name: "C++",
+      },
+      {
+        name: "OOP",
+      },
+      {
+        name: "UI/UX",
+      },
+    ];
+    const showModal = ref(false);
+    return {
+      workArray,
+      jobs,
+      removeAll,
+      education,
+      experience,
+      showModal,
+      skills,
+    };
   },
 };
 </script>
@@ -126,6 +254,7 @@ export default {
   width: 100%;
   max-width: min(1400px, 95vw);
   margin: 0 auto;
+  grid-gap: 20px;
   & > div {
     width: 100%;
   }
@@ -136,13 +265,8 @@ export default {
     flex-wrap: wrap;
     justify-content: center;
     grid-gap: 10px;
-    // max-width: 1400px;
-    // width: 100%;
 
-    // width: clamp(90%, 90vw, 1400px);
-    // padding: 20px;
     margin: 0 auto;
-    // padding: 50px;
     height: 100%;
 
     img {
@@ -150,6 +274,17 @@ export default {
       width: clamp(220px, 23vw, 400px);
       filter: grayscale(100%);
       border-radius: 50%;
+    }
+    .img {
+      // width: 40vw;
+      width: clamp(220px, 23vw, 400px);
+      aspect-ratio: 5/ 6;
+      filter: grayscale(100%);
+      border-radius: 60%;
+
+      background-position: center top;
+      background-repeat: no-repeat;
+      background-size: 150%;
     }
 
     .img-friend {
@@ -255,7 +390,7 @@ export default {
           .site-title {
             font-weight: bold;
           }
-          .description{
+          .description {
             color: #000a;
           }
           .tag-array {
@@ -277,6 +412,59 @@ export default {
           }
         }
       }
+    }
+  }
+
+  .resume-section {
+    display: flex;
+    grid-gap: 15px;
+    flex-direction: column;
+    justify-content: center;
+
+    .subtitle {
+      span {
+        font-size: 1.6em;
+        font-weight: bold;
+        color: white;
+        background: black;
+        padding: 0 5px;
+      }
+    }
+
+    .education-div {
+      display: flex;
+      grid-gap: 15px;
+      flex-direction: column;
+      justify-content: center;
+    }
+
+    .skills-div {
+      display: grid;
+      grid-gap: 15px;
+      grid-template-rows: repeat(auto-fit, 1fr);
+      // grid-template-columns: repeat(auto-fit, minmax(70px, 1fr));
+      grid-template-columns: repeat(3, 1fr);
+      place-items: center;
+    }
+
+    .contact-section {
+      display: flex;
+      grid-gap: 15px;
+      flex-direction: column;
+      justify-content: center;
+
+      i {
+        color: green;
+      }
+    }
+
+    .buttons {
+      max-width: 300px;
+      margin: 0 auto;
+      display: flex;
+      grid-gap: 15px;
+      flex-direction: rows;
+      justify-content: center;
     }
   }
 }
