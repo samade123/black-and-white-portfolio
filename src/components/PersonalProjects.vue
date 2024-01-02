9b84298597cff55c665d4f5111b567da4e76a01d
Samuel Ademola <samuel.ademola43@gmail.com>
2024-01-02 23:28:23 +0000

    details section added to personal projects section + skills

--- src/components/PersonalProjects.vue
+++ src/components/PersonalProjects.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="project-section section" id="work">
 
-    <h2 class="featured-section__title section__title">Personal Jobs</h2>
-    <div class="section-main">
-
-      <!-- <div class="project selected"></div>
-      <div class="project"></div>
-      <div class="project"></div>
-      <div class="project"></div> -->
+    <h2 class="featured-section__title section__title">Personal Projects</h2>
+    <div class="section-main" id="project-section-main">
 
       <div class="project" role='image' :aria-label='job.description' :title="job.name" v-for="job, index in jobs"
         :tabindex="job.selected ? false : '0'" @keypress.enter="selectJob(index)" :key="job.name"
         :class="{ 'selected': job.selected, [job.identifier ? 'project--' + job.identifier : '']: true }"
         @click="selectJob(index)" :style="{
           backgroundImage: `${job.bgImage}, linear-gradient(135deg, #${job.bgColors[0]} 30%, #${job.bgColors[1]} 100%)`,
-        }"></div>
+        }">
+        <div class="project-details">
+          <h3 class="project-details__title">{{ job.name }}</h3>
+          <h4 class="project-details__description">{{ job.description }}</h4>
+          <div class="project-details__case-study"></div>
+          <div class="project-details__skills">
+            <div class="project-details__skill" v-for='(skill, index) in job.tags' :key="index"
+              :class="skill.toLowerCase()">
+            </div>
+          </div>
+          <div class="project-details__images">
+            <div class="project-details__image"></div>
+          </div>
+
+        </div>
+      </div>
     </div>
   </div>
   <Modal :showModal="showModal" @closeModal="showModal = false" :data="selectedJob" />
@@ -23,7 +33,14 @@
 <script>
 import { widthFunction } from "@/composables/Mobile";
 import { computed, ref } from "@vue/reactivity";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
 import Modal from "@/components/Modal.vue";
+import { onMounted } from 'vue';
+gsap.registerPlugin(ScrollTrigger);
+
 
 export default {
   name: "WorkSection",
@@ -76,13 +93,14 @@ export default {
         github: "https://github.com/samade123/black-and-white-portfolio",
         image: require(`../assets/small/weather-iphone.webp`),
         imageLarge: require(`../assets/${getScreenCategory()}/weather-iphone.webp`),
-        selected: true,
+        selected: false,
         bgImage: 'url(' + require('@/assets/featured/weather-mobile.webp') + ')',
         // bgColors: ['e2e0e3', 'e2e0e3'],
         bgColors: ['eff1f2', '87b4c6'],
 
         identifier: 'weather',
-        tags: ["All", "Vue.js", "CSS", "HTML", "JAVASCRIPT"],
+        tags: ["All", "Vue-js", "CSS", "HTML", "JAVASCRIPT"],
+        // skills: [{"All"}, {"Vue.js"}, {"CSS"}"CSS", {}"HTML", {}"JAVASCRIPT"],
         otherFeatures: [
           "Color themeing (by weather)",
           "SASS",
@@ -90,9 +108,9 @@ export default {
         ],
       },
       {
-        name: "Presentation Viewer/Database",
+        name: "Presentation Viewer - Phase 1",
         description:
-          "Basic Design task required of me for a front end interview",
+          "A presentation viewer and database based off the Marp markup language; integrated with Notion database REST API",
         href: "",
         github: "https://github.com/samade123/FE-developer-exercise-reach-plc",
         image: require(`../assets/small/quick-design-task.webp`),
@@ -101,7 +119,7 @@ export default {
         identifier: 'cac',
         selected: false,
         bgImage: 'url(' + require('@/assets/featured/cac.webp') + ')',
-        tags: ["All", "CSS", "HTML", "JAVASCRIPT"],
+        tags: ["All", "CSS", "HTML", "Typescript", 'angular'],
         otherFeatures: ["Responsive Design"],
       },
       {
@@ -138,6 +156,25 @@ export default {
       },
     ]);
 
+    onMounted(() => {
+
+      setTimeout(() => {
+        var tl = gsap.timeline({ delay: 0.6, smoothChildTiming: true });
+        tl.call(() => {
+          selectJob(0)
+        }, null, 0)
+
+        ScrollTrigger.create({
+          start: "center bottom",
+          trigger: "#work",
+          endTrigger: ".project-section .section-main",
+          animation: tl,
+          toggleActions: "play pause resume none",
+          // once: true,
+        });
+      });
+    });
+
     const selectedJob = ref(jobs.value[0]);
 
     const openModal = (job) => {
@@ -162,15 +199,21 @@ export default {
 <style lang="scss" scoped>
 @use "@/styles/theme.scss" as *;
 @use "@/styles/style.scss" as *;
+@use "@/styles/project-details.scss" as *;
+
 
 .section {
   .section-main {
     display: grid;
-    grid-template-columns: 4fr 1fr 1fr 1fr 0fr 0fr 0fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr 0fr 0fr 0fr;
     gap: 0.5em;
-    height: 250px;
+    min-height: 250px;
     width: 100%;
     transition: grid-template-columns 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.125);
+
+    &.ready {
+      grid-template-columns: 4fr 1fr 1fr 1fr 0fr 0fr 0fr;
+    }
 
     &:has(.project:first-child.selected) {
       grid-template-columns: 4fr 1fr 1fr 1fr 0fr 0fr 0fr;
@@ -192,6 +235,7 @@ export default {
       background-color: var(--section-bg-color, blue);
       border-radius: 14px;
       outline-color: color-mix(in srgb, var(--section-bg-color) 20%, #0a0a0a);
+      height: 250px;
 
 
       @extend .selectable-grid;
@@ -208,81 +252,82 @@ export default {
       }
 
       &--weather {
-        --section-bg-color: #e0dfe1;
+        --section-bg-color: #87b4c6;
         background-position-x: -20px, 0;
         background-position-y: 100px, 0;
-        background-size: auto 250px, cover;
+        background-size: auto 80%, cover;
 
 
         &:hover:not(.selected),
-        &:focus-with:not(.selected) {
+        &:focus-within:not(.selected) {
           background-position-y: 95px, 0;
 
         }
 
         &.selected {
           background-position-y: 0px, 0;
-          background-size: auto 280px, cover;
+          background-size: auto 100%, cover;
         }
 
       }
 
       &--agency {
-        --section-bg-color: #e0dfe1;
+        --section-bg-color: #c6ba7e;
         background-position: 50% 30px, 0 0;
-        background-size: auto 250px;
+        background-size: auto 85%, cover;
 
 
 
         &:hover:not(.selected),
-        &:focus-with:not(.selected) {
+        &:focus-within:not(.selected) {
           background-position: 50% 30px, 0 0;
-          background-size: auto 280px;
+          background-size: auto 88%, cover;
         }
 
         &.selected {
           background-position: -33px center, 0 0;
-          background-size: auto 300px;
+          background-size: auto 90%, cover;
         }
       }
 
       &--cac {
-        --section-bg-color: #e0dfe1;
+        --section-bg-color: #e7dfdc;
         background-position: 50% 30px, 0 0;
-        background-size: auto 180px, cover;
+        background-size: auto 85%, cover;
 
 
 
         &:hover:not(.selected),
-        &:focus-with:not(.selected) {
+        &:focus-within:not(.selected) {
           background-position: 50% 30px, 0 0;
-          background-size: auto 200px, cover;
+          background-size: auto 88%, cover;
         }
 
         &.selected {
           background-position: 0 center, 0 0;
-          background-size: auto 250px, cover;
+          background-size: auto 90%, cover;
         }
       }
 
       &--figma {
         --section-bg-color: #e0dfe1;
         background-position: 50% 30px, 0 0;
-        background-size: auto 230px, cover;
+        background-size: auto 85%, cover;
 
 
 
         &:hover:not(.selected),
-        &:focus-with:not(.selected) {
+        &:focus-within:not(.selected) {
           background-position: 50% 30px, 0 0;
-          background-size: auto 250px, cover;
+          background-size: auto 88%, cover;
         }
 
         &.selected {
           background-position: -20px center, 0 0;
-          background-size: auto 260px, cover;
+          background-size: auto 90%, cover;
         }
       }
     }
   }
-}</style>
+}
+</style>
