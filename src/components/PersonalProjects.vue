5d17c1233792ff5371b341616034c0e4041f1d07
Samuel Ademola <samuel.ademola43@gmail.com>
2024-01-06 22:11:43 +0000

    working on the personal projects

--- src/components/PersonalProjects.vue
+++ src/components/PersonalProjects.vue
@@ -11,23 +11,44 @@
           backgroundImage: `${job.bgImage}, linear-gradient(135deg, #${job.bgColors[0]} 30%, #${job.bgColors[1]} 100%)`,
         }">
         <div class="project-details">
-          <h3 class="project-details__title">{{ job.name }}</h3>
-          <h4 class="project-details__description">{{ job.description }}</h4>
-          <div class="project-details__case-study"></div>
+          <h3 class="project-details__title"
+            :class="{ 'expand': job.expand['h3'].possible ? job.expand['h3'].state : false, 'expandible': job.expand['h3'].possible && !job.expand['h3'].state}" @click="expand(job, 'h3')">{{
+              job.name }}
+            <div class="project-details__expand" role="button" v-if="job.expand['h3'].possible">
+              <img src="@/assets/logos/expand.svg" v-svg-inline alt="" class="">
+            </div>
+          </h3>
+          <h4 class="project-details__description"
+            :class="{ 'expand': job.expand['h4'].possible ? job.expand['h4'].state : false, 'expandible': job.expand['h4'].possible && !job.expand['h4'].state }" @click="expand(job, 'h4')">{{
+              job.description }}
+            <div class="project-details__expand" role="button" v-if="job.expand['h4'].possible">
+              <img src="@/assets/logos/expand.svg" v-svg-inline alt="" class="">
+            </div>
+          </h4>
+          <!-- <div class="project-details__case-study"></div> -->
           <div class="project-details__skills">
             <div class="project-details__skill" v-for='(skill, index) in job.tags' :key="index"
               :class="skill.toLowerCase()">
             </div>
           </div>
-          <div class="project-details__images">
+          <!-- <div class="project-details__images">
             <div class="project-details__image"></div>
+          </div> -->
+
+          <div class="buttons-div">
+            <a :href="job.href">
+              <button class="buttons">Preview</button>
+            </a>
+            <a :href="job.github" target="_blank" rel="noopener">
+              <button class="buttons white">Source Code</button>
+            </a>
           </div>
 
         </div>
       </div>
     </div>
   </div>
-  <Modal :showModal="showModal" @closeModal="showModal = false" :data="selectedJob" />
+  <!-- <Modal :showModal="showModal" @closeModal="showModal = false" :data="selectedJob" /> -->
 </template>
 
 <script>
@@ -44,9 +65,9 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default {
   name: "WorkSection",
-  components: {
-    Modal,
-  },
+  // components: {
+  //   Modal,
+  // },
   setup() {
     const { width, setMobile, getScreenCategory } = widthFunction();
 
@@ -62,6 +83,7 @@ export default {
     ]);
 
     const showModal = ref(false);
+    let selectedJobIndex = 0
 
     const changeSelected = (pill) => {
       skillsArray.value.forEach((skill) => {
@@ -70,11 +92,32 @@ export default {
       pill.current = true;
     };
     const selectJob = (jobIndex) => {
+      resetJobs();
+      selectedJobIndex = jobIndex;
+      jobs.value[jobIndex].selected = true;
+    };
+
+    const expand = (job, key) => {
+
+      let keys = Object.keys(job.expand)
+      keys.forEach((key) => {
+        job.expand[key].state = false;
+      })
+
+      setTimeout(() => {
+
+        job.expand[key].state = true;
+      }, 100)
+
+
+
+    };
+
+    const resetJobs = () => {
       jobs.value.forEach((job) => {
         job.selected = false;
       });
-      jobs.value[jobIndex].selected = true;
-    };
+    }
 
     const removeAll = (tagArray) => {
       const allIndex = tagArray.indexOf("All");
@@ -106,13 +149,16 @@ export default {
           "SASS",
           "Adaptive Design",
         ],
+        expand: {
+          'h3': { possible: false }, 'h4': { possible: true }
+        }
       },
       {
         name: "Presentation Viewer - Phase 1",
         description:
           "A presentation viewer and database based off the Marp markup language; integrated with Notion database REST API",
-        href: "",
-        github: "https://github.com/samade123/FE-developer-exercise-reach-plc",
+        href: "https://cac-hymns.vercel.app/",
+        github: "https://github.com/samade123/angular_cac_hymns_in_marp",
         image: require(`../assets/small/quick-design-task.webp`),
         imageLarge: require(`../assets/${getScreenCategory()}/quick-design-task.webp`),
         bgColors: ['e7dfdc', 'e7dfdc'],
@@ -121,6 +167,9 @@ export default {
         bgImage: 'url(' + require('@/assets/featured/cac.webp') + ')',
         tags: ["All", "CSS", "HTML", "Typescript", 'angular'],
         otherFeatures: ["Responsive Design"],
+        expand: {
+          'h3': { possible: true }, 'h4': { possible: true }
+        }
       },
       {
         name: "Agency landing page - demo",
@@ -138,6 +187,9 @@ export default {
         bgImage: 'url(' + require('@/assets/featured/agency.webp') + ')',
         tags: ["All", "CSS", "HTML", "JAVASCRIPT"],
         otherFeatures: ["Responsive Design", "GSAP", "Animations"],
+        expand: {
+          'h3': { possible: false }, 'h4': { possible: true }
+        }
       },
       {
         name: "Market Black - Concept Design",
@@ -153,26 +205,67 @@ export default {
         selected: false,
         tags: ["All", "Figma", "Design"],
         otherFeatures: [],
+        expand: {
+          'h3': { possible: true }, 'h4': { possible: true }
+        }
       },
     ]);
+    let config = {
+      startTrigger: setMobile.value ? '.section--clients' : '.section-partial--codestacks',
+      start: setMobile.value ? 'bottom center' : "center center",
+    }
+
+    let setValues = () => {
+      config.startTrigger = setMobile.value ? '#work' : '.section-partial--codestacks';
+      config.start = setMobile.value ? 'top center' : "+=200 center";
+    }
 
     onMounted(() => {
 
+      setValues();
       setTimeout(() => {
-        var tl = gsap.timeline({ delay: 0.25, smoothChildTiming: true });
-        tl.call(() => {
-          selectJob(0)
-        }, null, 0)
+        // var tl = gsap.timeline({ smoothChildTiming: true });
+        // tl.call(() => {
+        //   selectJob(0)
+        // }, null, 0)
+
+        window.addEventListener("resize", () => {
+          setValues();
+          ScrollTrigger.refresh();
+        });
 
         ScrollTrigger.create({
-          start: "center bottom",
-          trigger: "#work",
-          endTrigger: ".project-section .section-main",
-          animation: tl,
+          start: config.start,
+          trigger: config.startTrigger,
+          end: "bottom+=100px center",
+          endTrigger: ".project-section#work",
+          // animation: tl,
           toggleActions: "play pause resume none",
-          // once: true,
+          // markers: true,
+          onEnter: () => {
+            setTimeout(() => {
+              selectJob(selectedJobIndex)
+            }, 0);
+          },
+          onLeave: () => {
+
+            setTimeout(() => {
+              resetJobs();
+            }, 150);
+          },
+          onLeaveBack: () => {
+            setTimeout(() => {
+              resetJobs();
+            }, 150);
+          },
+          onEnterBack: () => {
+            setTimeout(() => {
+              selectJob(selectedJobIndex)
+            }, 350);
+          },
         });
       }, 50);
+
     });
 
     const selectedJob = ref(jobs.value[0]);
@@ -192,6 +285,7 @@ export default {
       openModal,
       showModal,
       selectedPill,
+      expand,
     };
   },
 };
@@ -237,6 +331,22 @@ export default {
       border-radius: 14px;
       outline-color: color-mix(in srgb, var(--section-bg-color) 20%, #0a0a0a);
       height: 250px;
+
+      div[role="button"] {
+        --color-mix-ratio: 40%;
+        background-color: color-mix(in srgb, #cccc, var(--section-bg-color, blue) var(--color-mix-ratio));
+        border: 1px solid black;
+        border-radius: 5px;
+        transition: background-color 0.3s ease;
+        padding: 0.05em;
+
+        &:hover {
+          cursor: pointer;
+          --color-mix-ratio: 20%;
+
+        }
+
+      }
 
       &:hover:not(.selected),
       &:focus-within:not(.selected) {
@@ -328,4 +438,5 @@ export default {
     }
   }
 }
+
 @import "../styles/project-section-mobile.scss";</style>
